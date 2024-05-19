1aca884284ce70b222b2925bd54fe58bcd900323
ernelsn <rosalnelson.e@gmail.com>
2024-05-19 21:48:09 +0800

    Fixes #7 - Overall design can be further improved

--- src/stores/surveyStore.js
+++ src/stores/surveyStore.js
@@ -18,9 +18,7 @@ export const useSurveyStore = defineStore('survey', {
     },
     questionTypes: ["short answer", "paragraph", "multiple choice", "checkbox", "dropdown", "linear scale"],
     endTime: null,
-    timerId: null,
-    timeLeft: null,
-    now: Date.now(),
+    started: false,
   }),
 
   actions: {
@@ -43,6 +41,7 @@ export const useSurveyStore = defineStore('survey', {
         const res = await surveyService.fetchSurvey(id);
         this.currentSurvey.data = res.data.data;
         this.currentSurvey.loading = false;
+
         return res;
       } catch (err) {
         this.currentSurvey.loading = false;
@@ -51,42 +50,17 @@ export const useSurveyStore = defineStore('survey', {
     },
 
     async getSurveyBySlug(slug) {
-      this.resetTimer();
       this.currentSurvey.loading = true;
       try {
         const res = await surveyService.getSurveyBySlug(slug);
         this.currentSurvey.data = res.data.data;
-        let startTimeKey = 'startTime-' + slug;
-        let startTime = localStorage.getItem(startTimeKey);
-        if (startTime === null) {
-          startTime = Date.now();
-          localStorage.setItem(startTimeKey, startTime);
-        }
-        let timeLimit = Number(res.data.data.time_limit);
-        let endTime = Number(startTime) + timeLimit * 1000;
-        this.endTime = endTime;
         this.currentSurvey.loading = false;
-        let timerId = setInterval(() => {
-          this.now = Date.now();
-          if (this.timeLeft > 0) {
-            this.timeLeft--;
-          }
-        }, 1000);
-        this.timerId = timerId;
+        
         return res;
       } catch (err) {
         this.currentSurvey.loading = false;
         throw err;
       }
-    },
-
-    resetTimer() {
-      if (this.timerId !== null) {
-        clearInterval(this.timerId);
-      }
-      this.timerId = null;
-      this.endTime = null;
-      this.timeLeft = null;
     },
 
     async storeSurvey(survey) {
@@ -101,9 +75,11 @@ export const useSurveyStore = defineStore('survey', {
       }
       return response;
     },
+
     async storeSurveyAnswer({surveyId, answers}) {
       return surveyService.storeSurveyAnswer(surveyId, answers);
     },
+
     async deleteSurvey(id) {
       const res = await surveyService.deleteSurvey(id);
       await this.getSurveys();
@@ -111,7 +87,6 @@ export const useSurveyStore = defineStore('survey', {
     },
   },  
   persist: {
-    paths: ['endTime', 'timerId', 'timeLeft'],
+    paths: ['endTime', 'started'],
   },
 });
-    
